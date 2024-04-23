c699cf2979897fff9acbe320104fbef0ea891dae
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-04-23 17:30:48 +0300

    ContactsPage

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -3,13 +3,13 @@ import { useDispatch } from "react-redux";
 import DocumentTitle from "../components/DocumentTitle";
 import ContactForm from "../components/ContactForm";
 import ContactList from "../components/ContactList";
-import { apiGetUserContacts } from "../redux/contacts/operations";
+import { fetchContacts } from "../redux/contacts/operations";
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(apiGetUserContacts());
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
