de946622c74c5fe8d9c1a8bd77a8e205416e6858
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-04-23 20:20:32 +0300

    delete title

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,10 +1,8 @@
 import RegistrationForm from "../components/RegistrationForm";
-import DocumentTitle from "../components/DocumentTitle";
 
 export default function RegisterPage() {
   return (
     <div>
-      <DocumentTitle>Registration</DocumentTitle>
       <RegistrationForm />
     </div>
   );
